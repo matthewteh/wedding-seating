8e21416258e510cb3c8f11338847d63ee12bf310
Matthew Tan <76041610+matthewteh@users.noreply.github.com>
2026-01-18 16:49:04 +0800

    feat: installed node js and other packages

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/api/seat", (req, res) => {
+  const name = req.query.name;
+
+  const guests = [
+    { name: "John Tan", table: 6, seat: "B" },
+    { name: "Michelle Lim", table: 3, seat: "A" },
+  ];
+
+  const guest = guests.find((g) => g.name.toLowerCase() === name.toLowerCase());
+
+  if (guest) res.json(guest);
+  else res.status(404).json({ message: "Not found" });
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
