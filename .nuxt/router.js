654a09658661f9e7459a6de179d6bd15159a8131
Matthew Tan <76041610+matthewteh@users.noreply.github.com>
2026-02-06 17:40:30 +0800

    chore: added gh-pages for website to run on main

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -12,7 +12,7 @@ Vue.use(Router)
 
 export const routerOptions = {
   mode: 'history',
-  base: '/',
+  base: 'wedding-seating/',
   linkActiveClass: 'nuxt-link-active',
   linkExactActiveClass: 'nuxt-link-exact-active',
   scrollBehavior,
